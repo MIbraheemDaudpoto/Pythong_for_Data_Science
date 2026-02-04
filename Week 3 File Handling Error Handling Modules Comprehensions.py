253af07f84fd7bc28a4662d0aec3c71dbf99fc9e
Muhammad Ibraheem <codewithibf@gmail.com>
2026-02-05 00:00:50 +0500

    Update Week 3 File Handling Error Handling Modules Comprehensions.py

--- Week 3 File Handling Error Handling Modules Comprehensions.py
+++ Week 3 File Handling Error Handling Modules Comprehensions.py
@@ -1,15 +1,2 @@
 # File Handling
 
-
-# Opening a file in write mode
-
-# p=open(r'Week 1 Basics  Variables Data Types Input Output Loops Conditionals.py')
-
-# print(p.read())
-
-
-# Questions on File Handling
-
-
-
-
