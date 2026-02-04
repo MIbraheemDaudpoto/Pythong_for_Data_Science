a940c6ce958c06dc0eda6070e24323aa34472cfe
Muhammad Ibraheem <codewithibf@gmail.com>
2026-02-05 00:32:10 +0500

    Update Week 3 File Handling Error Handling Modules Comprehensions.py

--- Week 3 File Handling Error Handling Modules Comprehensions.py
+++ Week 3 File Handling Error Handling Modules Comprehensions.py
@@ -4,13 +4,54 @@
 
 
 
-# Opening a file in write mode
 
-# p=open(r'Week 1 Basics  Variables Data Types Input Output Loops Conditionals.py')
-
-# print(p.read())
+            # Questions on File Handling
 
 
 
+# # Opening a file and append some text inside that file with 'a'
 
+# try:
+#     p= open(r"C:\Users\ibrah\OneDrive\Desktop\New Text Document.txt",'a')
+#     # 'a' is used to append text in a file which is aready created on particular path just append something inside this without overlaping/Rewriting a file
+
+#     p.write("\n once again Hello This is new file and this overlaped")
+
+#     print(p.read())
+#     p.close()
+# except Exception as err:
+#     print(err)
+
+
+
+
+
+
+# # Opening a file and append some text inside that file with 'a'
+
+# try:
+#     p= open(r"C:\Users\ibrah\OneDrive\Desktop\New  Document.txt",'w')
+#     # 'w' is used to update file and remove previous content inside file, it will remove it and write what you have given. If file is not available at  path it will auto create file and add those text inside  this.
+
+
+#     p.write("\n once again Hello This is new file and this overlaped")
+
+#     print(p.read())
+#     p.close()
+# except Exception as err:
+#     print(err)
+
+
+
+
+# # Opening a file and append some text inside that file with 'r'
+
+# try:
+#     p= open(r"C:\Users\ibrah\OneDrive\Desktop\New  Document.txt",'r')
+#     # 'r' is used to read a file, file must exists.
+    
+#     print(p.read())
+#     p.close()
+# except Exception as err:
+#     print(err)
 
